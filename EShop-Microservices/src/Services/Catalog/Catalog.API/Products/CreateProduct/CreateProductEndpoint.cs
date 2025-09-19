ced2f92cb1a3c666a0acd466cb47773e7bd5bec0
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-09-19 15:40:50 -0300

    Developed CQRS with MeditaR Command and Query classes and Handlers, Developed Vertical Slice Product Folder with Endpoints and Handler. Used Carter to Simplify Minimal APIs and Mapster to Map request Content to Records. Registered Packages in Program.cs. Tested Create Product Endpoint.

---
+++ EShop-Microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>(); // mapster
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>(); // mapster
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create a Product");
+        }
+    }
+}
