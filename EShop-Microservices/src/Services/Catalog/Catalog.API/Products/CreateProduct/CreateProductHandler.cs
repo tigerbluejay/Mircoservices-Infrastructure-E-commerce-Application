32e09041c090f94eb78d1887704ac7bef3a48405
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-09-22 18:31:36 -0300

    Develop Product Endpoints and Handlers, Exception, and Configuration Files for Docker including the Database Setup with Postgresql

--- EShop-Microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ EShop-Microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -9,7 +9,7 @@ namespace Catalog.API.Products.CreateProduct
     public record CreateProductResult(Guid Id);
 
 
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -22,7 +22,10 @@ namespace Catalog.API.Products.CreateProduct
               Price = command.Price 
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
---
+++ EShop-Microservices/src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(Product product);
+
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            if (product is null) {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
