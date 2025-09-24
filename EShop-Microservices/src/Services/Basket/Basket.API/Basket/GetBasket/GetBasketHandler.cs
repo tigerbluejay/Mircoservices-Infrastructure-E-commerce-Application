2a21a0be927d51362ab49963f6ff49343da31943
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-09-24 06:26:58 -0300

    Developed Vertical Slice Architecture Features (Handlers and Endpoints) as well as Models for Basket.API, Implemented Repository Pattern for Basket, Exception Handling, Global Usings, General Configurations, and Docker Compose and Docker Override to add the BasketDb with Postgres

---
+++ EShop-Microservices/src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketQueryHandler(IBasketRepository basketRepository) : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            var basket = await basketRepository.GetBasket(query.UserName, cancellationToken);
+
+            return new GetBasketResult(basket);
+        }
+    }
+}
--- EShop-Microservices/src/Services/Basket/Basket.API/Program.cs
+++ EShop-Microservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,30 @@
+
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
 // Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+
+});
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
