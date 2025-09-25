eb1d5613574f448f241cd213805fcee1c720158c
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-09-25 14:15:03 -0300

    Implemented Proxy and Decorators Patterns with Scrutor Library on CachedBasketRepository, Set up Redis as Distributed Cache using docker-compose file for multi container docker environment

---
+++ EShop-Microservices/src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    // proxy pattern: CachedBasketRepository is a proxy to BasketRepository - forwarding the calls
+    // decorator pattern: CachedBasketRepository adds caching functionality to BasketRepository
+    public class CachedBasketRepository(IBasketRepository basketRepository, IDistributedCache cache) : IBasketRepository
+    {
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            await basketRepository.DeleteBasket(userName, cancellationToken);
+        
+            await cache.RemoveAsync(userName, cancellationToken);
+
+            return true;
+
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+            if (!string.IsNullOrEmpty(cachedBasket))
+            {
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+            }
+            var basket = await basketRepository.GetBasket(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await basketRepository.StoreBasket(basket, cancellationToken);
+            
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+    }
+}
--- EShop-Microservices/src/Services/Basket/Basket.API/Program.cs
+++ EShop-Microservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,6 @@
 
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,13 +19,31 @@ builder.Services.AddMarten(options =>
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
     options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+
+app.MapHealthChecks("/healthz",
+    new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
