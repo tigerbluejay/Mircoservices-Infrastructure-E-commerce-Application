469e0e3dea7d38179ede13f60de0a1868f1e74e2
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-09-29 14:02:49 -0300

    Created Project Libraries for Ordering Microservices, Established Project Dependencies, Introduced Depedency Injection Extension Methods on each Library

---
+++ EShop-Microservices/src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectiongString = configuration.GetConnectionString("Database");
+
+            // Add infrastructure services here, e.g., DbContext, Repositories, External Services, etc.
+            // services.AddDbContext<OrderingDbContext>(options =>
+            //     options.UseSqlServer(configuration.GetConnectionString("OrderingConnection")));
+
+            // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
