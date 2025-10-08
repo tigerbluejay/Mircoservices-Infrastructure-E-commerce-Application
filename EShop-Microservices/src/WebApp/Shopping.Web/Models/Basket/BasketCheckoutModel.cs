2afe3d2ed9993f8cb399a577681156aa4243fe8c
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-10-08 11:20:57 -0300

    Shopping.Web Client Part 1: launchSettings.json, Define Models, Services (using Refit to consume Yarp API Gateway Endpoints), add Global usings and Modify Program.cs - Generate css and images folder, and work on Shared Razor Pages and Index Razor Page

---
+++ EShop-Microservices/src/WebApp/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class BasketCheckoutModel
+    {
+        public string UserName { get; set; } = default!;
+        public Guid CustomerId { get; set; } = default!;
+        public decimal TotalPrice { get; set; }
+        
+        // Shipping and Billing Address
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string EmailAddress { get; set; } = default!;
+        public string AddressLine { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string State { get; set; } = default!;
+        public string ZipCode { get; set; } = default!;
+        
+        // Payment
+        public string CardName { get; set; } = default!;
+        public string CardNumber { get; set; } = default!;
+        public string Expiration { get; set; } = default!;
+        public string CVV { get; set; } = default!;
+        public int PaymentMethod { get; set; } = default!;
+    }
+
+    // wrapper classes
+
+    // parameter name must match the property name in CheckoutBasketEndpoint
+    public record CheckoutBasketRequest(BasketCheckoutModel BasketCheckoutDTO);
+    public record CheckoutBasketResponse(bool IsSuccess);
+}
