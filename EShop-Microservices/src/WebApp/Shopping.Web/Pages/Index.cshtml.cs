ea571af390c8ca2ef76d637a33742cc4bd16f27e
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-10-09 15:27:25 -0300

    Shopping.Web Client Part 2: Develop Razor Pages and Code Behind Files for Cart, Product Detail and Product List, Completed Index Code Behind File, and Added Method to the IBasketService Interface, Registered IBasketService in Program.cs

--- EShop-Microservices/src/WebApp/Shopping.Web/Pages/Index.cshtml.cs
+++ EShop-Microservices/src/WebApp/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace Shopping.Web.Pages
 {
-    public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger) 
+    public class IndexModel(ICatalogService catalogService, IBasketService basketService,
+        ILogger<IndexModel> logger) 
         : PageModel
     {
 
@@ -14,5 +17,28 @@ namespace Shopping.Web.Pages
             ProductList = result.Products;
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+        {
+            logger.LogInformation("Add to Cart button clicked");
+
+            var productResponse = await catalogService.GetProduct(productId);
+
+            var basket = await basketService.LoadUserBasket();
+
+            basket.Items.Add(new ShoppingCartItemModel
+            {
+                ProductId = productId,
+                ProductName = productResponse.Product.Name,
+                Price = productResponse.Product.Price,
+                Quantity = 1,
+                Color = "Black"
+            });
+
+            await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+            return RedirectToPage("Cart");
+        }
+
     }
 }
--- EShop-Microservices/src/WebApp/Shopping.Web/Services/IBasketService.cs
+++ EShop-Microservices/src/WebApp/Shopping.Web/Services/IBasketService.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Net;
+
 namespace Shopping.Web.Services
 {
     public interface IBasketService
@@ -18,5 +20,27 @@ namespace Shopping.Web.Services
 
         [Post("/basket-service/basket/checkout")]
         Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+        public async Task<ShoppingCartModel> LoadUserBasket()
+        {
+            var userName = "swn";
+            ShoppingCartModel basket;
+
+            try
+            {
+                var getBasketResponse = await GetBasket(userName);
+                basket = getBasketResponse.Cart;
+            }
+            catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
+            {
+                basket = new ShoppingCartModel
+                {
+                    UserName = userName,
+                    Items = []
+                };
+            }
+
+            return basket;
+        }
     }
 }
